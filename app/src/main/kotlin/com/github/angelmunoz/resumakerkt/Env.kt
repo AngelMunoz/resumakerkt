7f966f3c5cd34ca51c41b4440468a0ede4b6e537
Angel D. Munoz <scythes.lol@gmail.com>
2023-10-16 13:19:46 -0600

    add log level re-structure interfaces,

--- app/src/main/kotlin/com/github/angelmunoz/resumakerkt/Env.kt
+++ app/src/main/kotlin/com/github/angelmunoz/resumakerkt/Env.kt
@@ -1,25 +1,33 @@
 package com.github.angelmunoz.resumakerkt
 
-import com.github.angelmunoz.resumakerkt.services.getPdfConverter
-import com.github.angelmunoz.resumakerkt.services.getResumeLocator
-import com.github.angelmunoz.resumakerkt.services.getTemplateRenderer
-import com.github.angelmunoz.resumakerkt.types.PdfConverter
-import com.github.angelmunoz.resumakerkt.types.ResumeLocator
-import com.github.angelmunoz.resumakerkt.types.TemplateRenderer
+import arrow.core.raise.catch
+import com.github.angelmunoz.resumakerkt.services.*
+import com.github.angelmunoz.resumakerkt.types.*
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.pebbletemplates.pebble.PebbleEngine
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
-import org.jsoup.Jsoup
-import org.jsoup.helper.W3CDom
 import org.kodein.di.*
-import java.nio.file.Path
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import io.github.oshai.kotlinlogging.DelegatingKLogger
 
 @OptIn(ExperimentalSerializationApi::class)
-fun getAppEnv(): DI {
+fun getAppEnv(logLevel: LogLevel): DI {
+    val logger = KotlinLogging.logger("ResumakerKT")
+    val under = (logger as DelegatingKLogger<org.slf4j.Logger>).underlyingLogger as ch.qos.logback.classic.Logger
+    when (logLevel) {
+        LogLevel.Info -> under.level = Level.INFO
+        LogLevel.Debug -> under.level = Level.DEBUG
+        LogLevel.Trace -> under.level = Level.TRACE
+    }
+
     return DI {
+        bindInstance<String>(tag = "working-dir") { System.getProperty("user.dir") }
+        bindInstance<KLogger> { logger }
+        bindInstance<PebbleEngine> { PebbleEngine.Builder().build() }
         bindInstance<Json> {
             Json {
                 isLenient = true
@@ -27,39 +35,19 @@ fun getAppEnv(): DI {
                 explicitNulls = false
             }
         }
-        bindMultiton<String, KLogger> { name: String ->
-            KotlinLogging.logger(name)
-        }
-        bindInstance { PebbleEngine.Builder().build() }
-        bindInstance(tag = "cwd-file-locator") {
-            { path: String ->
-                val cwd = System.getProperty("user.dir")
-                Path.of(cwd, path).toAbsolutePath().toFile()
-            }
-        }
-        bindInstance(tag = "cwd-file-locator-ensure-dir") {
-            { path: String ->
-                val cwd = System.getProperty("user.dir")
-                val file = Path.of(cwd, path).toAbsolutePath().toFile()
-                file.parentFile.mkdirs()
-                file
-            }
+        bindSingleton<() -> PdfRendererBuilder> { { PdfRendererBuilder() } }
+        bindSingleton<DocumentParser> { getDocumentParser(instance()) }
+        bindSingleton<FileLocator> {
+            getFileLocator(instance(), instance("working-dir"))
         }
         bindSingleton<ResumeLocator> {
-            getResumeLocator(
-                instance(),
-                instance("cwd-file-locator")
-            )
+            getResumeLocator(instance(), instance())
         }
-        bindSingleton<TemplateRenderer> { getTemplateRenderer(instance(), instance(), instance("cwd-file-locator")) }
+        bindSingleton<TemplateRenderer> {
+            getTemplateRenderer(instance(), instance(), instance(), instance())
+        }
         bindSingleton<PdfConverter> {
-            getPdfConverter(
-                instance("cwd-file-locator-ensure-dir"),
-                { html -> Jsoup.parse(html).let { doc -> W3CDom().fromJsoup(doc) } },
-                { doc, os ->
-                    PdfRendererBuilder().withW3cDocument(doc, "/").toStream(os).run()
-                }
-            )
+            getPdfConverter(instance(), instance(), instance(), instance())
         }
     }
 }
