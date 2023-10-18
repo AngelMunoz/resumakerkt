4dc5e0d58c02013647d9fb375b9b5b8630d72112
Angel D. Munoz <scythes.lol@gmail.com>
2023-10-18 11:14:50 -0600

    Format services - move executor to a new interface

--- app/src/main/kotlin/com/github/angelmunoz/resumakerkt/services/Services.kt
+++ app/src/main/kotlin/com/github/angelmunoz/resumakerkt/services/Services.kt
@@ -1,11 +1,14 @@
 /**
- * This package could technically contain classes that implement the services
- * but for our purposes we'll just use functions if we ever need to use classes
- * (for stateful services) we can add them here as well
+ * This package contains the factory functions to provide services based on the interfaces defined in the types package
+ * For the most part, here is where the implementation those interfaces live, since we're primarily using
+ * Functional Interfaces (https://kotlinlang.org/docs/fun-interfaces.html) it usually contains functions.
+ * It can however also contain classes that implement those interfaces.
  */
 package com.github.angelmunoz.resumakerkt.services
 
 import arrow.core.Either
+import arrow.core.flatMap
+import arrow.core.getOrElse
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import com.github.angelmunoz.resumakerkt.types.*
@@ -18,121 +21,179 @@ import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.jsonObject
 import org.jsoup.Jsoup
 import org.jsoup.helper.W3CDom
+import java.io.File
 import java.io.StringWriter
-import java.nio.file.Path
 
 
-fun getTemplateRenderer(logger: KLogger, engine: PebbleEngine, json: Json, fileLocator: FileLocator): TemplateRenderer {
+private fun locateResumeTemplate(
+  logger: KLogger, fileLocator: IFileLocator, engine: PebbleEngine, templateNameOrPath: String
+): Either<TemplateRenderingError, PebbleTemplate> {
+  return either {
+    try {
+      if (templateNameOrPath.startsWith(".") && templateNameOrPath.endsWith(".html")) {
+        logger.debug { "Custom template specified: '$templateNameOrPath'" }
 
-    return TemplateRenderer { templateNameOrPath, resume ->
-        fun getTemplate(templateNameOrPath: String): Either<TemplateRenderingError, PebbleTemplate> {
-            return either {
-                try {
-                    if (templateNameOrPath.startsWith(".") && templateNameOrPath.endsWith(".html")) {
-                        logger.debug { "Custom template specified: '$templateNameOrPath'" }
-                        val file = fileLocator
-                                .find(templateNameOrPath, false)
-                                .mapLeft { TemplateRenderingError(it.error) }
-                                .bind()
-                        logger.debug { "File exists: ${file.exists()}" }
-                        engine.getTemplate(file.absolutePath)
-                    } else {
-                        logger.trace { "Using built-in template: '$templateNameOrPath'" }
-                        engine.getTemplate("templates/$templateNameOrPath")
-                    }
-                } catch (err: Error) {
-                    logger.trace(err) { "Failed to obtain the template from disk." }
-                    val error =
-                            TemplateRenderingError(err.message
-                                    ?: "Failed to obtain the Html Template: '$templateNameOrPath'")
-                    raise(error)
-                }
-            }
-        }
+        val file = fileLocator
+          .find(templateNameOrPath, false)
+          .mapLeft { TemplateRenderingError(it.error) }
+          .bind()
 
-        StringWriter().use { writer ->
-            either {
-                val template = getTemplate(templateNameOrPath).bind()
+        logger.trace { "File exists: ${file.exists()}" }
 
-                val payload = json.encodeToJsonElement(resume).jsonObject.toMap()
-                try {
-                    template.evaluate(writer, payload)
-                } catch (error: Error) {
-                    logger.trace(error) { "Attempted to evaluate and failed with $resume" }
-                    raise(TemplateRenderingError(error.message ?: "Failed to evaluate the template"))
-                }
-                writer.toString()
-            }
-        }
+        engine.getTemplate(file.absolutePath)
+      } else {
+        logger.trace { "Using built-in template: '$templateNameOrPath'" }
+
+        engine.getTemplate("templates/$templateNameOrPath")
+      }
+    } catch (err: Error) {
+      logger.trace(err) { "Failed to obtain the template from disk." }
+      val error = TemplateRenderingError(
+        err.message ?: "Failed to obtain the Html Template: '$templateNameOrPath'"
+      )
+      raise(error)
     }
+  }
 }
 
-fun getResumeLocator(json: Json, fileLocator: FileLocator): ResumeLocator {
-    return ResumeLocator { path ->
-        either {
-            val resume = fileLocator.find(path, false).mapLeft { ResumeError(it.error) }.bind()
-            json.decodeFromString(resume.readText())
+fun getTemplateRenderer(
+  logger: KLogger,
+  engine: PebbleEngine,
+  json: Json,
+  fileLocator: IFileLocator
+): ITemplateRenderer {
+
+  return ITemplateRenderer { templateNameOrPath, resume ->
+
+    StringWriter().use { writer ->
+      either {
+        val template = locateResumeTemplate(logger, fileLocator, engine, templateNameOrPath).bind()
+
+        val payload = json.encodeToJsonElement(resume).jsonObject.toMap()
+        try {
+          template.evaluate(writer, payload)
+        } catch (error: Error) {
+          logger.trace(error) { "Attempted to evaluate and failed with $resume" }
+          raise(TemplateRenderingError(error.message ?: "Failed to evaluate the template"))
         }
+        writer.toString()
+      }
     }
+  }
 }
 
-fun getFileLocator(logger: KLogger, cwd: String): FileLocator {
-    return FileLocator { path, ensureParent ->
-        either {
-            val file = Path.of(cwd, path).toAbsolutePath().toFile()
-            try {
-                ensure(file.isFile) { FileLocatorError("Path '${file.absolutePath} is not a file") }
-
-                if (ensureParent)
-                    file.parentFile.mkdirs()
-            } catch (error: Error) {
-                logger.trace(error) { "Failed to create parent directory" }
-                raise(FileLocatorError(error.message ?: "Failed to create parent directory"))
-            }
-            file
-        }
+fun getResumeLocator(json: Json, fileLocator: IFileLocator): IResumeLocator {
+  return IResumeLocator { path ->
+    either {
+      val resume = fileLocator
+        .find(path, false)
+        .mapLeft { ResumeError(it.error) }
+        .bind()
+      json.decodeFromString(resume.readText())
     }
+  }
 }
 
-fun getDocumentParser(logger: KLogger): DocumentParser {
-    return DocumentParser { html ->
-        either {
-            try {
-                Jsoup.parse(html).let { doc -> W3CDom().fromJsoup(doc) }
-            } catch (error: Error) {
-                logger.trace(error) { "Failed to parse HTML Document" }
-                raise(error.message ?: "Failed to parse HTML Document")
-            }
+fun getFileLocator(logger: KLogger, cwd: String): IFileLocator {
+  return IFileLocator { path, ensureParent ->
+    either {
+
+      val file = File(cwd).resolve(path)
+      try {
+        if (ensureParent) {
+          logger.trace { "Ensuring parent directory exists" }
+          file.parentFile.mkdirs()
         }
+
+        ensure(!file.isDirectory) { FileLocatorError("Path '${file.absolutePath} is not a file") }
+      } catch (error: Error) {
+        logger.trace(error) { "Failed to create parent directory" }
+        raise(FileLocatorError(error.message ?: "Failed to create parent directory"))
+      }
+      file
     }
+  }
+}
+
+fun getDocumentParser(logger: KLogger): IDocumentParser {
+  return IDocumentParser { html ->
+    either {
+      try {
+        Jsoup.parse(html).let { doc -> W3CDom().fromJsoup(doc) }
+      } catch (error: Error) {
+        logger.trace(error) { "Failed to parse HTML Document" }
+        raise(error.message ?: "Failed to parse HTML Document")
+      }
+    }
+  }
 }
 
 fun getPdfConverter(
-        logger: KLogger,
-        fileLocator: FileLocator,
-        documentParser: DocumentParser,
-        pdfRenderer: () -> PdfRendererBuilder,
-): PdfConverter {
-    return PdfConverter { html, outPath ->
-        either {
-            val document = documentParser
-                    .parse(html)
-                    .mapLeft { PdfConvertError(it) }
-                    .bind()
-            val file = fileLocator
-                    .find(outPath, true)
-                    .mapLeft { PdfConvertError(it.error) }.bind()
-            try {
-                file.outputStream().use { os ->
-                    logger.debug { "Rendering document " }
+  logger: KLogger,
+  fileLocator: IFileLocator,
+  documentParser: IDocumentParser,
+  pdfRenderer: () -> PdfRendererBuilder,
+): IPdfConverter {
+  return IPdfConverter { html, outPath ->
+    either {
+      val document = documentParser
+        .parse(html)
+        .mapLeft { PdfConvertError(it) }
+        .bind()
+      val file = fileLocator
+        .find(outPath, true)
+        .mapLeft { PdfConvertError(it.error) }
+        .bind()
+      try {
+        file.outputStream().use { os ->
+          logger.debug { "Rendering document" }
 
-                    pdfRenderer().withW3cDocument(document, "/").toStream(os).run()
-                }
-            } catch (error: Error) {
-                logger.trace(error) { "Failed to write the final PDF" }
-                raise(PdfConvertError(error.message ?: "Failed to write the final PDF"))
-            }
-            file.absolutePath.toString()
+          pdfRenderer().withW3cDocument(document, "/").toStream(os).run()
         }
+      } catch (error: Error) {
+        logger.trace(error) { "Failed to write the final PDF" }
+        raise(PdfConvertError(error.message ?: "Failed to write the final PDF"))
+      }
+      file.absolutePath.toString()
     }
+  }
+}
+
+
+fun getResumeGenerator(
+  logger: KLogger,
+  resumeLocator: IResumeLocator,
+  templateRenderer: ITemplateRenderer,
+  pdfConverter: IPdfConverter
+): IResumeGenerator {
+  return IResumeGenerator { resumePath, outDir, template, language ->
+    logger.info { "Generating pdf files from '$resumePath'" }
+    val resumeList = resumeLocator.getResume(resumePath).mapLeft { it.error }.getOrElse { emptyList() }.run {
+      if (language.isEmpty()) {
+        logger.info { "No language provided, generating all available languages." }
+        this
+      } else {
+        this.filter { resume ->
+          language.contains(resume.language.name)
+        }
+      }
+    }
+
+    for (resume in resumeList) {
+      logger.info { "Generating resumes for languages: ${resume.language.name}" }
+      val renderResult = templateRenderer.render(template, resume).mapLeft { it.error }.flatMap { htmlContent ->
+        pdfConverter.convert(
+          html = htmlContent,
+          outPath = "${outDir}/${resume.language.name}.pdf"
+        ).mapLeft { it.error }
+      }
+      when (renderResult) {
+        is Either.Right -> logger.info { "Generated PDF at: '${renderResult.value}" }
+        is Either.Left -> logger.error { "Unable to generate PDF: ${renderResult.leftOrNull()}" }
+      }
+    }
+
+    logger.info { "Processed '${resumeList.size}' files." }
+
+  }
 }
